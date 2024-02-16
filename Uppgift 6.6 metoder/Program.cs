fde3155d739a9fa56c446846a7801055790408f6
HaZ3R0 <megamackan@outlook.com>
2024-02-16 10:49:59 +0100

    Add project files.

---
+++ Uppgift 6.6 metoder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace bang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening");
+            string[] mening = Console.ReadLine().Split(' ');
+            Varannat(mening);
+        }
+
+        static void Varannat(string[] mening)
+        {
+            bool varannat = true; 
+            
+            foreach (string s in mening)
+            {
+                if (varannat)
+                {
+                    Console.Write(s.ToUpper() + " ");
+                }
+                else
+                {
+                    Console.Write(s.ToLower() + " ");
+                }
+
+                varannat = !varannat;
+            }
+        }
+
+    }
+}
